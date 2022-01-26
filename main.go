b4f1377c2209c0613426507be4e682c7d906ad87
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-01-26 17:37:33 -0500

    create output package

--- main.go
+++ main.go
@@ -2,74 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"time"
-
-	oto "github.com/hajimehoshi/oto/v2"
 
 	"github.com/rbren/midi/pkg/midi"
+	"github.com/rbren/midi/pkg/output"
 )
 
-type MusicPlayer struct {
-	buffer   []byte
-	readPos  *int
-	writePos *int
-}
-
-func NewMusicPlayer(capacity int) MusicPlayer {
-	zero := 0
-	return MusicPlayer{
-		buffer:  make([]byte, capacity),
-		readPos: &zero,
-	}
-}
-
-func (m *MusicPlayer) incrementReadPos() {
-	*m.readPos++
-	if *m.readPos >= len(m.buffer) {
-		*m.readPos = 0
-	}
-}
-
-func (m *MusicPlayer) incrementWritePos() {
-	*m.writePos++
-	if *m.writePos >= len(m.buffer) {
-		*m.writePos = 0
-	}
-}
-
-func (m MusicPlayer) Read(p []byte) (n int, err error) {
-	numRead := 0
-	for idx := range p {
-		p[idx] = m.buffer[*m.readPos]
-		m.incrementReadPos()
-		numRead++
-	}
-	return numRead, nil
-}
-
-func (m MusicPlayer) Write(p []byte) (n int, err error) {
-	numWritten := 0
-	for _, b := range p {
-		m.buffer[*m.writePos] = b
-		m.incrementWritePos()
-		numWritten++
-	}
-	return numWritten, nil
-}
-
-func startOutputBuffer() io.Writer {
-	sampleRate := 48000
-	bufferSize := sampleRate * 10
-	ctx, _, err := oto.NewContext(sampleRate, 2, 2)
-	must(err)
-	musicPlayer := NewMusicPlayer(bufferSize)
-	player := ctx.NewPlayer(musicPlayer)
-	player.Play()
-	return musicPlayer
-}
-
-// This example reads from the first input and and writes to the first output port
 func main() {
 	notes := make(chan midi.MidiNote, 1000)
 	done := make(chan bool)
@@ -77,7 +14,10 @@ func main() {
 		err := midi.StartDriver(notes, done)
 		must(err)
 	}()
-	startOutputBuffer()
+
+	out, err := output.NewOutputLine(48000)
+	must(err)
+	out.Player.Play()
 
 	for {
 		select {
@@ -85,9 +25,6 @@ func main() {
 			fmt.Println("rec note", note)
 		}
 	}
-	time.Sleep(10000 * time.Second)
-	// Output: got channel.NoteOn channel 0 key 60 velocity 100
-	// got channel.NoteOff channel 0 key 60
 }
 
 func must(err error) {
---
+++ pkg/output/player.go
@@ -0,0 +1,75 @@
+package output
+
+import (
+	oto "github.com/hajimehoshi/oto/v2"
+)
+
+type OutputLine struct {
+	sampleRate int
+	Line       MusicReaderWriter
+	Player     oto.Player
+}
+
+func NewOutputLine(sampleRate int) (*OutputLine, error) {
+	line := NewMusicReaderWriter(sampleRate * 10)
+	ctx, _, err := oto.NewContext(sampleRate, 2, 2)
+	if err != nil {
+		return nil, err
+	}
+	return &OutputLine{
+		sampleRate: sampleRate,
+		Line:       line,
+		Player:     ctx.NewPlayer(line),
+	}, nil
+}
+
+type MusicReaderWriter struct {
+	buffer   []byte
+	readPos  *int
+	writePos *int
+	player   oto.Player
+}
+
+func NewMusicReaderWriter(capacity int) MusicReaderWriter {
+	readPos := 0
+	writePos := 0
+	return MusicReaderWriter{
+		buffer:   make([]byte, capacity),
+		readPos:  &readPos,
+		writePos: &writePos,
+	}
+}
+
+func (m *MusicReaderWriter) incrementReadPos() {
+	*m.readPos++
+	if *m.readPos >= len(m.buffer) {
+		*m.readPos = 0
+	}
+}
+
+func (m *MusicReaderWriter) incrementWritePos() {
+	*m.writePos++
+	if *m.writePos >= len(m.buffer) {
+		*m.writePos = 0
+	}
+}
+
+func (m MusicReaderWriter) Read(p []byte) (n int, err error) {
+	numRead := 0
+	for idx := range p {
+		p[idx] = m.buffer[*m.readPos]
+		m.incrementReadPos()
+		numRead++
+	}
+	return numRead, nil
+}
+
+func (m MusicReaderWriter) Write(p []byte) (n int, err error) {
+	numWritten := 0
+	for _, b := range p {
+		m.buffer[*m.writePos] = b
+		m.incrementWritePos()
+		numWritten++
+	}
+	return numWritten, nil
+}
