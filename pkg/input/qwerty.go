79a3cf10509e9257576b3e022afaba21bd8a8522
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-01-27 20:50:05 -0500

    omething like keypress

--- pkg/input/qwerty.go
+++ pkg/input/qwerty.go
@@ -2,9 +2,15 @@ package input
 
 import (
 	"os"
+	"time"
 
 	"github.com/nsf/termbox-go"
 )
+
+type pressEvent struct {
+	lastTime time.Time
+	inputKey InputKey
+}
 
 type QwertyKeyboard struct{}
 
@@ -13,7 +19,31 @@ func (k QwertyKeyboard) StartListening() (chan InputKey, error) {
 	if err != nil {
 		return nil, err
 	}
+	onKeys := map[termbox.Key]pressEvent{}
 	notes := getOutputChannel()
+
+	ticker := time.NewTicker(10 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				tickTime := time.Now()
+				toDelete := []termbox.Key{}
+				for key, evt := range onKeys {
+					diff := tickTime.Sub(evt.lastTime)
+					if diff > 250*time.Millisecond {
+						evt.inputKey.Action = "channel.NoteOff"
+						notes <- evt.inputKey
+						toDelete = append(toDelete, key)
+					}
+				}
+				for _, del := range toDelete {
+					delete(onKeys, del)
+				}
+			}
+		}
+	}()
+
 	go func() {
 		for {
 			switch ev := termbox.PollEvent(); ev.Type {
@@ -22,8 +52,16 @@ func (k QwertyKeyboard) StartListening() (chan InputKey, error) {
 					k.Close()
 					os.Exit(0)
 				}
-				notes <- InputKey{Action: "channel.NoteOn"}
+				press := pressEvent{
+					lastTime: time.Now(),
+					inputKey: InputKey{Action: "channel.NoteOn"},
+				}
+				onKeys[ev.Key] = press
+				notes <- press.inputKey
 				termbox.Flush()
+			case termbox.EventInterrupt:
+				k.Close()
+				os.Exit(0)
 			case termbox.EventError:
 				panic(ev.Err)
 			}
