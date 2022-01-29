ce74d0411439eebadf0b0b007f34d2a4f32d46b5
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-01-29 15:33:11 -0500

    add writing to server

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -3,29 +3,41 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 
 	"github.com/rbren/midi/pkg/input"
 	"github.com/rbren/midi/pkg/logger"
+	"github.com/rbren/midi/pkg/music"
 )
+
+var sendInterval = 10 * time.Millisecond
 
 var upgrader = websocket.Upgrader{} // use default options
 
-type Message struct {
+type MessageIn struct {
 	Key    string
 	Action string
 }
 
+type MessageOut struct {
+	Time uint64
+}
+
 type Server struct {
-	notes chan input.InputKey
+	notes      chan input.InputKey
+	connection *websocket.Conn
+	Player     *music.MusicPlayer
 }
 
 func (s Server) StartListening() (chan input.InputKey, error) {
 	s.notes = make(chan input.InputKey, 20)
-	http.HandleFunc("/echo", s.echo)
+	http.HandleFunc("/connect", s.connect)
 	http.Handle("/", http.FileServer(http.Dir("web")))
 	go http.ListenAndServe(":8080", nil)
+	go s.startReadLoop()
+	go s.startWriteLoop()
 	fmt.Println("started listening")
 	return s.notes, nil
 }
@@ -34,16 +46,23 @@ func (s Server) Close() error {
 	return nil
 }
 
-func (s Server) echo(w http.ResponseWriter, r *http.Request) {
+func (s Server) connect(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		logger.ForceLog("upgrade:", err)
 		return
 	}
 	defer c.Close()
+	s.connection = c
+}
+
+func (s Server) startReadLoop() {
 	for {
-		msg := Message{}
-		err := c.ReadJSON(&msg)
+		if s.connection == nil {
+			continue
+		}
+		msg := MessageIn{}
+		err := s.connection.ReadJSON(&msg)
 		fmt.Println("msg", msg)
 		if err != nil {
 			logger.ForceLog("server read error:", err)
@@ -66,5 +85,25 @@ func (s Server) echo(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Println("send", inputKey)
 		s.notes <- inputKey
+	}
+}
+
+func (s Server) startWriteLoop() {
+	ticker := time.NewTicker(sendInterval)
+	for {
+		select {
+		case <-ticker.C:
+			if s.connection == nil {
+				continue
+			}
+			msg := MessageOut{
+				Time: s.Player.CurrentSample,
+			}
+			err := s.connection.WriteJSON(msg)
+			if err != nil {
+				logger.ForceLog("server write error:", err)
+				break
+			}
+		}
 	}
 }
