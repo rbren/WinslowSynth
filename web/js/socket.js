0b1e9d18b65156e9e8d7203f0f2845aae156e639
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-01-29 19:04:22 -0500

    fix buzz problem\, refactor

---
+++ web/js/socket.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function(evt) {
+  var ws = new WebSocket("ws://" + window.location.host + "/connect");
+  ws.onopen = function(evt) {
+    console.log("OPEN");
+  }
+  ws.onclose = function(evt) {
+    console.log("CLOSE");
+    ws = null;
+  }
+  ws.onmessage = function(evt) {
+    //console.log("RESPONSE: " + evt.data);
+  }
+  ws.onerror = function(evt) {
+    console.log("ERROR: " + evt.data);
+  }
+
+  const pressedKeys = {}
+
+  document.onkeydown = function(event) {
+    if (!ws) return true;
+    if (pressedKeys[event.key]) return true;
+    pressedKeys[event.key] = true;
+    console.log("DOWN: ", event.key)
+    ws.send(JSON.stringify({
+      "Key": event.key,
+      "Action": "down",
+     }))
+    return true;
+  }
+
+  document.onkeyup = function(event) {
+    if (!ws) return true;
+    pressedKeys[event.key] = false;
+    console.log("UP: ", event.key)
+    ws.send(JSON.stringify({
+      "Key": event.key,
+      "Action": "up",
+     }))
+    return true;
+  }
+});
