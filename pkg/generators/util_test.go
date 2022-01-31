557fa7ce1d2f85d3cb742aaad7adaf1e1f2025ae
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-01-31 10:52:01 -0500

    fix tests

--- pkg/generators/util_test.go
+++ pkg/generators/util_test.go
@@ -9,16 +9,17 @@ import (
 func TestGetConstants(t *testing.T) {
 	i := Warbler()
 	cs := GetConstants(i)
-	assert.Equal(t, 2, len(cs))
+	assert.Equal(t, 3, len(cs))
 	assert.Equal(t, "Warble Amt", cs[0].Name)
 	assert.Equal(t, "Warble Speed", cs[1].Name)
+	assert.Equal(t, "Frequency", cs[2].Name)
 }
 
 func TestSetConstant(t *testing.T) {
 	var i Generator
 	i = Warbler()
 	cs := GetConstants(i)
-	assert.Equal(t, 2, len(cs))
+	assert.Equal(t, 3, len(cs))
 	assert.Equal(t, float32(20.0), cs[0].Value)
 	i = SetConstant(i, "Warble Amt", 100.0)
 	cs = GetConstants(i)
