fc9cbb8a2fdc3ee6aadd71840983d553d0a80ec7
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2022-02-02 12:50:37 -0500

    fix tests

--- pkg/generators/util_test.go
+++ pkg/generators/util_test.go
@@ -9,19 +9,27 @@ import (
 func TestGetConstants(t *testing.T) {
 	i := Warbler()
 	cs := GetConstants(i)
-	assert.Equal(t, 3, len(cs))
-	assert.Equal(t, "Warble Amt", cs[0].Info.Name)
-	assert.Equal(t, "Warble Speed", cs[1].Info.Name)
-	assert.Equal(t, "Frequency", cs[2].Info.Name)
+	assert.Equal(t, 12, len(cs))
+}
+
+func findWarbleAmt(cs []Constant) *Constant {
+	for idx, c := range cs {
+		if c.Info.Name == "Warble Amt" {
+			return &cs[idx]
+		}
+	}
+	return nil
 }
 
 func TestSetConstant(t *testing.T) {
 	var i Generator
 	i = Warbler()
 	cs := GetConstants(i)
-	assert.Equal(t, 3, len(cs))
-	assert.Equal(t, float32(20.0), cs[0].Value)
-	i = SetConstant(i, "Warble Amt", 100.0)
+	warbleConst := findWarbleAmt(cs)
+	assert.NotEqual(t, nil, warbleConst)
+	assert.Equal(t, float32(20.0), warbleConst.Value)
+	i = SetConstant(i, "", "Warble Amt", 100.0)
 	cs = GetConstants(i)
-	assert.Equal(t, float32(100.0), cs[0].Value)
+	warbleConst = findWarbleAmt(cs)
+	assert.Equal(t, float32(100.0), warbleConst.Value)
 }
